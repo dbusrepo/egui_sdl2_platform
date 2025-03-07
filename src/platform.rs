74e7d3a07179b0b84ac706be6084528f29bf8f75
dbus <dbusrepo@gmail.com>
2025-03-07 17:27:10 +0100

    Update egui to 0.31.1

--- src/platform.rs
+++ src/platform.rs
@@ -258,15 +258,15 @@ impl Platform {
     ) -> anyhow::Result<egui::FullOutput> {
         // Get the egui output
         let output = self.egui_ctx.end_pass();
+
         // Update the clipboard
-        if !output.platform_output.copied_text.is_empty() {
-            // Get the copied text
-            let text = output.platform_output.copied_text.clone();
-            // Update the clipboard
-            video
-                .clipboard()
-                .set_clipboard_text(&text)
-                .map_err(|e| anyhow::anyhow!("Failed to assign text to clipboard: {}", e))?;
+        for command in &output.platform_output.commands {
+            if let egui::OutputCommand::CopyText(text) = command {
+                video
+                    .clipboard()
+                    .set_clipboard_text(&text)
+                    .map_err(|e| anyhow::anyhow!("Failed to assign text to clipboard: {}", e))?;
+            }
         }
 
         if let Some(cursor) = &mut self.cursor {
